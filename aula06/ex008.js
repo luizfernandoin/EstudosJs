1d6ff927209e5585b442346ea2d763a239a3d484
Luiz Fernando <luizfernandoinfo100@gmail.com>
2022-09-22 08:05:25 -0300

    commit inicial

---
+++ aula06/ex008.js
@@ -0,0 +1,9 @@
+var idade = 66
+
+if (idade < 16) {
+    console.log(`Não vota!`)
+} else if (idade >= 16 && idade < 18 || idade > 65){
+    console.log(`Voto opicional!`)
+} else {
+    console.log(`Voto Obrigatorio!`)
+}
